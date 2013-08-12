4e1a57bdcc79a2e0a35954f7f60b2df9cfe46683
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-08-12 18:34:51 -0300

    Basic search API

--- bireme/main/api.py
+++ bireme/main/api.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+
 from tastypie.resources import ModelResource
+from tastypie.utils import trailing_slash
 from tastypie import fields
 import models
+import requests
 
 
 class ResourceAPI(ModelResource):
@@ -12,6 +17,30 @@ class ResourceAPI(ModelResource):
         allowed_methods = ['get']
         resource_name = 'resource'
 
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/search%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_search'), name="api_get_search"),
+        ]
+
+    
+    def get_search(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)        
+
+        q = request.GET.get('q', '')
+
+        # url
+        search_url = "%siahx-controller/" % settings.SEARCH_SERVICE_URL
+
+        search_params = {'site': 'lis', 'col': 'main','op': 'search', 
+                    'output': 'site', 'lang': 'pt', 'q': q }
+
+        r = requests.get(search_url, params=search_params)
+
+        self.log_throttled_access(request)
+        return self.create_response(request, r.json())
+        
 class DescriptorResource(ModelResource):    
     resource = fields.ToOneField('main.api.ResourceAPI', 'resource')
 
