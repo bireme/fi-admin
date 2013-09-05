e47aa2a970122b5f266b06bbf3dfe460ac6bb8cb
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-09-05 09:17:48 -0300

    Exclude parameter is not necessary when field is defined when not editable

--- bireme/utils/admin.py
+++ bireme/utils/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import *
 
 class GenericAdmin(admin.ModelAdmin):
-    exclude = ('created', 'creator', 'updated', 'updater')
+    exclude = ()
 
     def save_model(self, request, obj, form, change):
         if hasattr(obj, 'updater') and hasattr(obj, 'creator'):
