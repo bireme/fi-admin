88e1cda4c80b3f6835f12a0696b6f92f5fd791bc
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-10-23 16:36:27 -0200

    Fix and add new fields to migration procedures

--- proc/gera_fixtures_config.sh
+++ proc/gera_fixtures_config.sh
@@ -1,9 +1,17 @@
 
+# config fixtures
 mx config from=1 count=1 pft=@fixture_thematic_area.pft -all now > thematic_area_iso.json
-
 mx config from=2 count=1 pft=@fixture_source_language.pft -all now > source_language_iso.json
-
 mx config from=3 count=1 pft=@fixture_source_type.pft -all now > source_type_iso.json
+
+# gizmo for source language
+mx config from=2 count=1 "pft=(if p(v317) then v317^c,'|',f(iocc,1,0) fi/)" now > g_source_language.seq
+mx seq=g_source_language.seq create=g_source_language -all now
+
+# gizmo for source type
+mx config from=3 count=1 "pft=(if p(v318) then v318^e,'|',f(iocc,1,0) fi/)" now > g_source_type.seq
+mx seq=g_source_type.seq create=g_source_type -all now
+
 
 # convert to utf8
 
@@ -14,3 +22,4 @@ iconv -f iso-8859-1 -t utf-8 source_language_iso.json > source_language.json
 iconv -f iso-8859-1 -t utf-8 source_type_iso.json > source_type.json
 
 rm *_iso.json
+rm *.seq
