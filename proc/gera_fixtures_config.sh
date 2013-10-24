ef9a8eac58b92c2ba79ee1819ca7070c6805a304
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-10-24 12:43:51 -0200

    Procedure for migration of resource thematic field [302]

--- proc/gera_fixtures_config.sh
+++ proc/gera_fixtures_config.sh
@@ -4,13 +4,16 @@ mx config from=1 count=1 pft=@fixture_thematic_area.pft -all now > thematic_area
 mx config from=2 count=1 pft=@fixture_source_language.pft -all now > source_language_iso.json
 mx config from=3 count=1 pft=@fixture_source_type.pft -all now > source_type_iso.json
 
+# gizmo for thematic area
+mx config from=1 count=1 "pft=(if p(v300) then v300^c,'|',f(iocc,1,0) fi/)" now > g_thematic_area.seq
+mx seq=g_thematic_area.seq create=g_thematic_area -all now
+
 # gizmo for source language
 mx config from=2 count=1 "pft=(if p(v317) then v317^c,'|',f(iocc,1,0) fi/)" now > g_source_language.seq
 mx seq=g_source_language.seq create=g_source_language -all now
 
 # gizmo for source type
 mx config from=3 count=1 "pft=(if p(v318) then v318^e,'|',f(iocc,1,0) fi/)" now > g_source_type.seq
-
 mx seq=g_source_type.seq gizmo=gansna,1 create=g_source_type -all now
 
 
--- proc/gera_fixtures_resource.sh
+++ proc/gera_fixtures_resource.sh
@@ -1,14 +1,17 @@
 
-mx lis pft=@fixture_resources.pft gizmo=gizmo_fixture lw=8000 from=1 count=50 now > sample_resources_iso.json
+mx lis pft=@fixture_resources.pft gizmo=gansna,318 gizmo=gizmo_fixture gizmo=g_source_language,317 gizmo=g_source_type,318  lw=8000 from=1 count=50 now > sample_resources_iso.json
 
-mx lis pft=@fixture_descriptors.pft gizmo=gizmo_fixture lw=8000 from=1 count=50 now > sample_descriptors_iso.json
+mx1660 lis pft=@fixture_descriptors.pft gizmo=gizmo_fixture lw=8000 from=1 count=50 now > sample_descriptors_iso.json
 
 mx lis pft=@fixture_keywords.pft gizmo=gizmo_fixture lw=8000 from=1 count=50 now > sample_keywords_iso.json
+
+mx lis pft=@fixture_resource_thematic.pft gizmo=gizmo_fixture gizmo=g_thematic_area,302 lw=8000 from=1 count=50 now > sample_resource_thematic_iso.json
 
 # convert to utf8
 
 iconv -f iso-8859-1 -t utf-8 sample_resources_iso.json > sample_resources.json
 iconv -f iso-8859-1 -t utf-8 sample_descriptors_iso.json > sample_descriptors.json
 iconv -f iso-8859-1 -t utf-8 sample_keywords_iso.json > sample_keywords.json
+iconv -f iso-8859-1 -t utf-8 sample_resource_thematic_iso.json > sample_resource_thematic.json
 
 rm *_iso.json
