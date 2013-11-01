52eeacf0b49b24a34a964b498534d66e11b0c19b
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-11-01 17:18:54 -0200

    Fix conversion format - set null for primary key (avoid duplication)

--- proc/gera_fixtures_resource.sh
+++ proc/gera_fixtures_resource.sh
@@ -3,11 +3,11 @@ mxcp lis create=lis_clean clean
 
 mx lis_clean pft=@fixture_resources.pft gizmo=gansna,318 gizmo=gizmo_fixture gizmo=g_source_language,317 gizmo=g_source_type,318  lw=8000 now > resources_iso.json
 
-mx1660 lis_clean pft=@fixture_descriptors.pft gizmo=gizmo_fixture lw=8000 from=1 count=50 now > descriptors_iso.json
+mx1660 lis_clean pft=@fixture_descriptors.pft gizmo=gizmo_fixture lw=8000 now > descriptors_iso.json
 
-mx lis_clean pft=@fixture_keywords.pft gizmo=gizmo_fixture lw=8000 from=1 count=50 now > keywords_iso.json
+mx lis_clean pft=@fixture_keywords.pft gizmo=gizmo_fixture lw=8000 now > keywords_iso.json
 
-mx lis_clean pft=@fixture_resource_thematic.pft gizmo=gizmo_fixture gizmo=g_thematic_area,302 lw=8000 from=1 count=50 now > resource_thematic_iso.json
+mx lis_clean pft=@fixture_resource_thematic.pft gizmo=gizmo_fixture gizmo=g_thematic_area,302 lw=8000 now > resource_thematic_iso.json
 
 # convert to utf8
 
