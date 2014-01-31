0703b803c776d7ae6043a4064e9b293a084a2ca7
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2014-01-31 18:13:03 -0200

    Migration and conversion procedures for LIS fields (305 and 314)

--- proc/gera_fixtures_resource.sh
+++ proc/gera_fixtures_resource.sh
@@ -1,7 +1,7 @@
 
 mxcp lis create=lis_clean clean
 
-mx lis_clean pft=@fixture_resources.pft gizmo=gansna,318 gizmo=gizmo_fixture gizmo=g_source_language,317 gizmo=g_source_type,318  lw=8000 now > resources_iso.json
+mx lis_clean pft=@fixture_resources.pft gizmo=gansna,318 gizmo=gizmo_fixture gizmo=g_record_source,305 gizmo=g_originator_location,314 gizmo=countries,314  gizmo=g_source_language,317 gizmo=g_source_type,318  lw=8000 now > resources_iso.json
 
 mx1660 lis_clean pft=@fixture_descriptors.pft gizmo=gizmo_fixture lw=8000 now > descriptors_iso.json
 
