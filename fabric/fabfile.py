cde2dfedbfab90780709bdceb3abc19f8e147f70
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2014-09-02 14:27:19 -0300

    Fabric file adjusts to run for multiples environments

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,70 +1,92 @@
 # -*- coding: utf-8 -*-
 import os
-from fabric.api import env, local, settings, abort, run, cd
+import sys
+from fabric.api import env, local, settings, abort, run, cd, task
 from fabric.operations import local, put, sudo, get
 from fabric.context_managers import prefix
 
-env.appname = 'lis-service'
+# Define common directories
+GIT_DIR = '/git'
+ROOT_DIR = '/bireme'
+ENV_DIR = '/env'
 
-env.user = ''
-env.path = ''
-env.rootpath = env.path + 'bireme/'
-env.gitpath = env.path + 'git/'
-env.virtualenv = env.path + 'env/'
-
-def test():
-    """Test server"""
-    env.hosts = ['']
-
-def stage():
-    """Stage server"""
-    env.hosts = ['']
-
-def production():
-    """Main server"""
-    env.hosts = ['']
-
-# including local environment from fabric
+# include stages servers information
 try: 
-    from environment import *
+    from environment import STAGES
 except: 
     pass
 
+# Function that load to env variable the settings from STAGES
+def stage_set(stage_name='test'):
+    env.stage = stage_name
+    for option, value in STAGES[env.stage].items():
+        setattr(env, option, value)
 
+    if not 'git_path' in STAGES[env.stage]:
+        env.git_path = env.path + GIT_DIR
+    if not 'root_path' in STAGES[env.stage]:
+        env.root_path = env.path + ROOT_DIR
+    if not 'virtualenv' in STAGES[env.stage]:
+        env.virtualenv = env.path + ENV_DIR
+
+@task
+def test():
+    """
+    set test server
+    """
+    stage_set('test')
+
+@task
+def production():
+    """
+    set production server
+    """
+    stage_set('production')
+
+
+@task
 def requirements():
     """
-        Install the requirements
+    install/update requirements
     """
-    with cd(env.gitpath):
+    with cd(env.git_path):
         with prefix('. %s/bin/activate' % env.virtualenv):
             run('pip install -r requirements.txt')
 
+@task
 def migrate():
     """
-        Realiza migration local
+    run manage.py migrate
     """    
-    with cd(env.rootpath):
+    with cd(env.root_path):
         with prefix('. %s/bin/activate' % env.virtualenv):
             run('python manage.py migrate')
 
+@task
 def restart_app():
     """
-        Restarts remote wsgi.
+    touch appliction.wsgi file (reload appliction)
     """
-    with cd(os.path.join(env.gitpath ,'fabric')):
+    with cd(os.path.join(env.git_path ,'fabric')):
         run("./restart.sh")
 
+@task
 def update_version_file():
-    with cd(env.rootpath):
+    """
+    update application version.txt based on git describe command
+    """
+
+    with cd(env.root_path):
         run("git describe --tags | cut -f 1,2 -d - > templates/version.txt")
         # traz o arquivo gerado da versão para minha máquina, e implementa a versão localmente
         get("templates/version.txt", "../bireme/templates")
 
+@task
 def full_update():
     """
-        Requirements, git pull, touch 
+    update requirements, execute git pull, touch application.wsgi
     """
-    with cd(env.gitpath):
+    with cd(env.git_path):
         run("git pull")
 
     requirements()
@@ -72,11 +94,12 @@ def full_update():
     update_version_file()
     restart_app()
 
+@task
 def update():
     """
-        Git pull e touch 
+    execute git pull and restart application
     """
-    with cd(env.gitpath):
+    with cd(env.git_path):
         run("git pull")
 
     update_version_file()
