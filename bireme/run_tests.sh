76e60ede88082436edaa1b41d7f8c3daf5599dbe
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2014-09-03 16:28:45 -0300

    Adjust run_tests shell script

--- bireme/run_tests.sh
+++ bireme/run_tests.sh
@@ -1,13 +1,7 @@
 # run tests
 
-echo "Runing tests from [main]"
-python manage.py test -v 0 main
-
-echo "Runing tests from [events]"
-python manage.py test -v 0 events
-
-echo "Runing tests from [suggest]"
-python manage.py test -v 0 suggest
-
-echo "Runing tests from [multimedia]"
-python manage.py test -v 0 multimedia
+for app in main events suggest multimedia
+do
+    echo "Runing tests from [$app]"
+    python -W ignore manage.py test -v 0 $app
+done
