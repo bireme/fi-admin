3b2dbfad615af1f7031c63e369334925431f0839
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2014-09-03 16:16:52 -0300

    Add suggest module at run_tests script

--- bireme/run_tests.sh
+++ bireme/run_tests.sh
@@ -6,6 +6,8 @@ python manage.py test -v 0 main
 echo "Runing tests from [events]"
 python manage.py test -v 0 events
 
+echo "Runing tests from [suggest]"
+python manage.py test -v 0 suggest
+
 echo "Runing tests from [multimedia]"
 python manage.py test -v 0 multimedia
-
