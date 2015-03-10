fdbed7fc31908993c6a27be0844e814ebd9a91e8
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2015-03-10 17:09:49 -0300

    Add migration to new tables AuxCode and AuxCodeLocal

---
+++ bireme/utils/migrations/0002_auxcode_auxcodelocal.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('utils', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AuxCode',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_time', models.DateTimeField(auto_now_add=True, verbose_name='created at')),
+                ('updated_time', models.DateTimeField(auto_now=True, verbose_name='updated', null=True)),
+                ('code', models.CharField(max_length=15, verbose_name='Code')),
+                ('field', models.CharField(max_length=35, verbose_name='Field name')),
+                ('language', models.CharField(max_length=10, verbose_name='Language', choices=[(b'en', 'English'), (b'pt-br', 'Portuguese'), (b'es', 'Spanish')])),
+                ('label', models.CharField(max_length=255, verbose_name='Name')),
+                ('created_by', models.ForeignKey(related_name='+', blank=True, editable=False, to=settings.AUTH_USER_MODEL, null=True)),
+                ('updated_by', models.ForeignKey(related_name='+', blank=True, editable=False, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': 'auxiliary code',
+                'verbose_name_plural': 'auxiliary codes',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AuxCodeLocal',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('language', models.CharField(max_length=10, verbose_name='language', choices=[(b'en', 'English'), (b'pt-br', 'Portuguese'), (b'es', 'Spanish')])),
+                ('label', models.CharField(max_length=255, verbose_name='name')),
+                ('auxcode', models.ForeignKey(verbose_name='Source type', to='utils.AuxCode')),
+            ],
+            options={
+                'verbose_name': 'Translation',
+                'verbose_name_plural': 'Translations',
+            },
+            bases=(models.Model,),
+        ),
+    ]
