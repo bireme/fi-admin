4e2f20159096fd98fdf02a246fcce87eea7ad5c8
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2016-05-23 15:17:40 -0300

    [biblioref] Implementa validação eletronic_address OR attachement for Sas. #99, #159

--- bireme/biblioref/cross_validation.py
+++ bireme/biblioref/cross_validation.py
@@ -3,8 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 from django.forms.utils import ErrorList
 from utils.forms import is_valid_for_publication
 
+import re
 
-def check_url_or_attachment(form, formset_attachment):
+def validate_page_or_eletronic_address(form, formset_attachment):
     """
     Check if is present the electronic_address field or attachment and run LILACS validation
     across pages, electronic_address, record_type and descriptive_information atribute_a
@@ -45,11 +46,41 @@ def check_url_or_attachment(form, formset_attachment):
     return url_or_attachment
 
 
+def check_url_or_attachment(form, formset_attachment):
+    """
+    Check if is present the electronic_address field or attachment
+    """
+    url_or_attachment = True
+    electronic_address = form.cleaned_data.get('electronic_address')
+
+    # if not electronic_address field check for attachment files
+    if not electronic_address:
+        for form_attach in formset_attachment:
+            try:
+                if form_attach.cleaned_data and form_attach.cleaned_data.get('DELETE') == False:
+                    electronic_address = True
+            except AttributeError:
+                # annoyingly, if a subform is invalid Django explicity raises
+                # an AttributeError for cleaned_data
+                pass
+
+    if not electronic_address:
+        formset_attachment.non_form_errors = ErrorList([_('Eletronic address or fulltext file required')])
+        formset_attachment.is_valid = False
+        url_or_attachment = False
+
+    return url_or_attachment
+
+
 def check_for_publication(form, formsets, user_data):
     """
     Run additional validation across forms fields for status LILACS-Express and LILACS
     """
     valid = True
+    # regex match starts with S (Serial) and ends with (as) analytic
+    regex_sas = r"^S.*as$"
+    Sas_record = re.search(regex_sas, form.document_type)
+
     status = form.cleaned_data['status']
     user_role = user_data['service_role'].get('LILDBI')
 
@@ -57,8 +88,8 @@ def check_for_publication(form, formsets, user_data):
     if status == 1 and form.document_type != 'S':
         valid = is_valid_for_publication(form, [formsets['descriptor'], formsets['thematic']])
 
-    # for analytic is required electronic_address or page field
-    if valid and 'a' in form.document_type and status != -1:
+    # for is_LILACS and journal article (Sas record) is required electronic_address or fulltext file #159
+    if valid and form.is_LILACS and Sas_record and status != -1:
         # check for electronic_address/attachment or page
         valid = check_url_or_attachment(form, formsets['attachment'])
 
