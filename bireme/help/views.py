264dc15e678da974396c66e807c7e4b3fe39aa0f
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2017-01-05 12:45:42 -0200

    [help] Adiciona possibilidade de formatar uma ajuda como página. #205

--- bireme/help/views.py
+++ bireme/help/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.conf import settings
 
@@ -8,7 +8,14 @@ def view_help(request, source_param, fieldname_param):
     """
     Receive source and field name and display help text
     """
+    page = request.GET.get('page', None)
+    output = {}
+
     help = get_object_or_404(Help, source=source_param, field=fieldname_param)
     help_html = help.get_help()
 
-    return HttpResponse(help_html)
+    if page:
+        output['help'] = help_html
+        return render(request, 'help/page.html', output)
+    else:
+        return HttpResponse(help_html)
