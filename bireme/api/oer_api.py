530a616a4f030468a09b8757bad9345b12142545
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2018-03-02 10:33:41 -0300

    [oer] Adiciona API

---
+++ bireme/api/oer_api.py
@@ -0,0 +1,87 @@
+# coding: utf-8
+from django.conf import settings
+from django.conf.urls import patterns, url, include
+
+from django.contrib.contenttypes.models import ContentType
+
+from tastypie.resources import ModelResource
+from tastypie.serializers import Serializer
+from tastypie.utils import trailing_slash
+from tastypie import fields
+
+from oer.models import OER
+
+from  main.models import Descriptor, ResourceThematic
+import requests
+import urllib
+
+class OERResource(ModelResource):
+
+    class Meta:
+        queryset = OER.objects.filter(status=1)
+        allowed_methods = ['get']
+        serializer = Serializer(formats=['json', 'xml'])
+        resource_name = 'oer'
+        filtering = {
+            'thematic_area_id': 'exact',
+        }
+        include_resource_uri = False
+
+    def build_filters(self, filters=None):
+        orm_filters = super(OERResource, self).build_filters(filters)
+
+        if 'thematic_area_id' in filters:
+            orm_filters['thematics__thematic_area__exact'] = filters['thematic_area_id']
+        return orm_filters
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/search%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_search'), name="api_get_search"),
+        ]
+
+
+    def get_search(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
+        q = request.GET.get('q', '')
+        fq = request.GET.get('fq', '')
+        start = request.GET.get('start', '')
+        count = request.GET.get('count', '')
+        lang = request.GET.get('lang', 'pt')
+        op = request.GET.get('op', 'search')
+        id = request.GET.get('id', '')
+        sort = request.GET.get('sort', 'created_date desc')
+
+        # filter result by approved resources (status=1)
+        if fq != '':
+            fq = '(django_ct:oer.oer) AND %s' % fq
+        else:
+            fq = '(django_ct:oer.oer)'
+
+        # url
+        search_url = "%siahx-controller/" % settings.SEARCH_SERVICE_URL
+
+        search_params = {'site': 'fi', 'col': 'main','op': op,'output': 'site', 'lang': lang,
+                    'q': q , 'fq': fq,  'start': start, 'count': count, 'id' : id,'sort': sort}
+
+        print search_params
+        
+        r = requests.post(search_url, data=search_params)
+
+        self.log_throttled_access(request)
+        return self.create_response(request, r.json())
+
+
+    def dehydrate(self, bundle):
+        c_type = ContentType.objects.get_for_model(bundle.obj)
+
+        descriptors = Descriptor.objects.filter(object_id=bundle.obj.id, content_type=c_type)
+        thematic_areas = ResourceThematic.objects.filter(object_id=bundle.obj.id, content_type=c_type, status=1)
+
+        # add fields to output
+        bundle.data['descriptors'] = [{'text': descriptor.text, 'code': descriptor.code} for descriptor in descriptors]
+        bundle.data['thematic_areas'] = [{'code': thematic.thematic_area.acronym, 'text': thematic.thematic_area.name} for thematic in thematic_areas]
+
+        return bundle
---
+++ bireme/oer/search_indexes.py
@@ -0,0 +1,73 @@
+import datetime
+from haystack import indexes
+from main.models import Descriptor, Keyword, SourceLanguage, ResourceThematic
+from attachments.models import Attachment
+from models import *
+
+import json
+
+from django.contrib.contenttypes.models import ContentType
+
+class OERIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title')
+    learning_objectives = indexes.CharField(model_attr='learning_objectives')
+    description = indexes.CharField(model_attr='description', null=True)
+    author = indexes.MultiValueField()
+    contributors = indexes.MultiValueField()
+    descriptor = indexes.MultiValueField()
+    link = indexes.MultiValueField()
+    created_date = indexes.CharField()
+    updated_date = indexes.CharField()
+    status = indexes.IntegerField(model_attr='status')
+
+    def get_model(self):
+        return OER
+
+    def prepare_author(self, obj):
+        if obj.creator:
+            return self.get_field_values(obj.creator)
+
+    def prepare_contributors(self, obj):
+        if obj.contributor:
+            return self.get_field_values(obj.contributor)
+
+    def prepare_descriptor(self, obj):
+        return [descriptor.code for descriptor in Descriptor.objects.filter(object_id=obj.id, content_type=ContentType.objects.get_for_model(obj), status=1)]
+
+
+    def prepare_link(self, obj):
+        electronic_address = []
+
+        urls = [oer.url for oer in OERURL.objects.filter(oer=obj.id)]
+        if urls:
+            electronic_address.extend(urls)
+
+        attachments = Attachment.objects.filter(object_id=obj.id,
+                                                content_type=ContentType.objects.get_for_model(obj))
+
+        for attach in attachments:
+            view_url = "%sdocument/view/%s" % (settings.SITE_URL,  attach.short_url)
+            electronic_address.append(view_url)
+
+        if electronic_address:
+            return electronic_address
+
+    def prepare_created_date(self, obj):
+        if obj.created_time:
+            return obj.created_time.strftime('%Y%m%d')
+
+    def prepare_updated_date(self, obj):
+        if obj.updated_time:
+            return obj.updated_time.strftime('%Y%m%d')
+
+    def get_field_values(self, field, attribute = 'text'):
+        value_list = field
+        if type(field) != list:
+            value_list = json.loads(field)
+
+        return [occ.get(attribute) for occ in value_list]
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(created_time__lte=datetime.datetime.now())
