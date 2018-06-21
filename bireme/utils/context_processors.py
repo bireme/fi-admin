24f3026c29bf2ec28e6d34d8dff10f5d66bfd85c
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2018-06-21 11:46:28 -0300

    [admin] Adiciona opção no menu para o Django Admin e permite vários usuários com perfil superuser (listas controladas)

--- bireme/utils/context_processors.py
+++ bireme/utils/context_processors.py
@@ -16,23 +16,19 @@ def additional_user_info(request):
     service_list = []
 
     if user.is_authenticated():
-        if not user.is_superuser:
-            user_data = simplejson.loads(user.profile.data)
-            user_cc = user_data['cc']
-            networks = user_data['networks']
-            ccs = user_data['ccs']
+        user_data = simplejson.loads(user.profile.data)
+        user_cc = user_data['cc']
+        networks = user_data['networks']
+        ccs = user_data['ccs']
 
-            # create a dictionary of service/role
-            for sr in user_data['service_role']:
-                service_id = sr.keys()[0]
-                role_id = sr.values()[0]
-                service_role[service_id] = role_id
+        # create a dictionary of service/role
+        for sr in user_data['service_role']:
+            service_id = sr.keys()[0]
+            role_id = sr.values()[0]
+            service_role[service_id] = role_id
 
-                if service_id not in service_list:
-                    service_list.append(service_id)
-
-        else:
-            user_cc = 'br1.1'
+            if service_id not in service_list:
+                service_list.append(service_id)
 
     return {'user_cc': user_cc, 'networks': networks, 'ccs': ccs, 'service_role': service_role,
             'user_name': user.username, 'user_id' : str(user.id), 'service_list' : service_list}
