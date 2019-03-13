4c7e811199dacbe2ad0a6b96af33715494f71d1d
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2019-03-13 12:56:35 -0300

    Ajustes menores

--- bireme/utils/urls.py
+++ bireme/utils/urls.py
@@ -3,4 +3,5 @@ from views import *
 
 urlpatterns = [
     url(r'^field_assist/(?P<field_name>\w+)/', field_assist, name='field_assist'),
+    url(r'^decs_suggestion/', decs_suggestion, name='decs_suggestion'),
 ]
