3f416070b79f21f8cb7f2adec9e08c30cb9fbd85
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2019-07-31 17:48:45 -0300

    Adiciona campo de user_type no additional_user_info

--- bireme/utils/context_processors.py
+++ bireme/utils/context_processors.py
@@ -9,6 +9,7 @@ def additional_user_info(request):
 
     user = request.user
     user_role = ''
+    user_type = ''
     user_cc = ''
     networks = ''
     ccs = ''
@@ -16,22 +17,24 @@ def additional_user_info(request):
     service_list = []
 
     if user.is_authenticated():
-        user_data = simplejson.loads(user.profile.data)
-        user_cc = user_data['cc']
-        networks = user_data['networks']
-        ccs = user_data['ccs']
+        if user.profile.data:
+            user_data = simplejson.loads(user.profile.data)
+            user_cc = user_data.get('cc')
+            networks = user_data.get('networks')
+            ccs = user_data.get('ccs')
+            user_type = user_data.get('user_type')
 
-        # create a dictionary of service/role
-        for sr in user_data['service_role']:
-            service_id = sr.keys()[0]
-            role_id = sr.values()[0]
-            service_role[service_id] = role_id
+            # create a dictionary of service/role
+            for sr in user_data['service_role']:
+                service_id = sr.keys()[0]
+                role_id = sr.values()[0]
+                service_role[service_id] = role_id
 
-            if service_id not in service_list:
-                service_list.append(service_id)
+                if service_id not in service_list:
+                    service_list.append(service_id)
 
-    return {'user_cc': user_cc, 'networks': networks, 'ccs': ccs, 'service_role': service_role,
-            'user_name': user.username, 'user_id' : str(user.id), 'service_list' : service_list}
+    return {'user_cc': user_cc, 'user_type': user_type, 'user_name': user.username, 'user_id' : str(user.id),
+            'networks': networks, 'ccs': ccs, 'service_role': service_role, 'service_list' : service_list}
 
 def django_settings(request):
     """
