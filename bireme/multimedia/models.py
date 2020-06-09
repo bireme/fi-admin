068487cee0815a9a4eadaff52d6efd9155c0a455
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2020-06-09 16:14:53 -0300

    [multimedia] Remove filtro por usuário devido a problemas de performance e adiciona cache

--- bireme/multimedia/models.py
+++ bireme/multimedia/models.py
@@ -2,6 +2,7 @@
 from django.utils.translation import ugettext_lazy as _, get_language
 from django.db import models
 from django.utils import timezone
+from django.core.cache import cache
 
 from utils.models import Generic, Country
 from main.choices import LANGUAGES_CHOICES
@@ -33,12 +34,18 @@ class MediaType(Generic):
 
     def __unicode__(self):
         lang_code = get_language()
-        translation = MediaTypeLocal.objects.filter(media_type=self.id, language=lang_code)
-        if translation:
-            return translation[0].name
-        else:
-            return self.name
+        cache_id = "multimedia_mediatype-{}-{}".format(lang_code, self.id)
+        mediatype_local = cache.get(cache_id)
+        if not mediatype_local:
+            translation = MediaTypeLocal.objects.filter(media_type=self.id, language=lang_code)
+            if translation:
+                mediatype_local = translation[0].name
+            else:
+                mediatype_local = self.name
 
+            cache.set(cache_id, mediatype_local, None)
+
+        return mediatype_local
 
 class MediaTypeLocal(models.Model):
 
