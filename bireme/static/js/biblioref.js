8ad09c6beb6c9226b135c3574a2699774245401c
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2020-11-30 12:18:09 -0200

    [biblioref] Ajuste para o link de consulta ao Portal DeCS. #1165

--- bireme/static/js/biblioref.js
+++ bireme/static/js/biblioref.js
@@ -3,34 +3,27 @@ function consult_DeCS(field_id, lang){
     var decs_url = "https://decs.bvsalud.org/" + lang + "/ths?filter=ths_exact_term_bool";
 
     $('#' + field_id + ' option:selected').each(function() {
-        search_exp += '"' + $(this).text() + '" OR ';
+        var current_term = $(this).text();
+
+        // remove addiotinal information of term ex. Child (6 to 12 years old)
+        if (current_term.includes(' (')){
+            current_term = current_term.substring(0, current_term.indexOf('(')-1);
+        }
+
+        search_exp += '"' + current_term + '" OR ';
     });
 
    if (search_exp == "") {
       return;
    }else{
       search_exp = search_exp.substring(0, search_exp.length - 4);
-      search_exp = retira_acentos(search_exp);
+
       decs_url +=  "&q=" + search_exp;
    }
 
    open_window(decs_url);
 }
 
-
-function retira_acentos(palavra) {
-   com_acento = 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ';
-   sem_acento = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
-   nova='';
-   for(i=0;i<palavra.length;i++) {
-       if (palavra.substr(i,1) != '.' &&  com_acento.search(palavra.substr(i,1))>=0) {
-           nova+=sem_acento.substr(com_acento.search(palavra.substr(i,1)),1);
-       } else {
-           nova+=palavra.substr(i,1);
-       }
-   }
-   return nova;
-}
 
 function open_window(url){
     var w = 785;    // window width
