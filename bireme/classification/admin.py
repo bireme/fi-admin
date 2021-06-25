caf5b91659463e4915cef33a47b625252b05ff44
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2021-06-25 16:44:05 -0300

    [admin] Adiciona opção para limpar cache na lista controlada de comunidades/coleções. #1233

--- bireme/classification/admin.py
+++ bireme/classification/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.cache import cache
 
 from classification.models import *
 
@@ -14,6 +15,28 @@ class CollectionAdmin(admin.ModelAdmin):
     inlines = [CollectionLocalAdmin, ]
     list_filter = (('country', admin.RelatedOnlyFieldListFilter),'community_flag',)
     ordering = ('parent',)
+    actions = ['clear_cache']
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+
+    def clear_cache(self, request, queryset):
+        for collection in queryset:
+            for cache_lang in ['pt', 'es', 'en']:
+                cache_id_c = "classification_collection-{}-{}".format(cache_lang, collection.id)
+                cache_id_f = "classification_collection_fullpath-{}-{}".format(cache_lang, collection.id)
+                try:
+                    cache.delete(cache_id_c)
+                    cache.delete(cache_id_f)
+                except:
+                    pass
+
+    clear_cache.short_description = 'Clear cache of selected communities/collections'
+
 
 
 admin.site.register(Collection, CollectionAdmin)
