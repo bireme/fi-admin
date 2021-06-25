2091d6c4af461c40c2305576eef47f88e047aab9
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2021-06-25 16:43:29 -0300

    [admin] Adiciona opção para limpar cache na lista controlada de bases. #1232

--- bireme/database/admin.py
+++ bireme/database/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from utils.admin import GenericAdmin
+from django.core.cache import cache
 
 from database.models import *
 
@@ -13,6 +14,25 @@ class DatabaseAdmin(GenericAdmin):
     inlines = [DatabaseLocalAdmin, ]
     search_fields = list_display = ['acronym', 'name']
     list_filter = ('regional_index', 'network_index', 'cc_index')
+    actions = ['clear_cache']
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+
+    def clear_cache(self, request, queryset):
+        for db in queryset:
+            for cache_lang in ['pt', 'es', 'en']:
+                cache_id = "database-{}-{}".format(cache_lang, db.id)
+                try:
+                    cache.delete(cache_id)
+                except:
+                    pass
+
+    clear_cache.short_description = 'Clear cache of selected databases'
 
 
 admin.site.register(Database, DatabaseAdmin)
