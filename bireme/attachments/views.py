cdea917e355e501fc972baa469baa7d925de2c82
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2021-07-05 15:43:49 -0300

    [attachments] Condição temporária para validação de ambiente python3. #1241

--- bireme/attachments/views.py
+++ bireme/attachments/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.conf import settings
 from short_url import decode_url
+from datetime import datetime, timezone
 
 from attachments.models import Attachment
 
@@ -13,6 +14,11 @@ def view_document(request, short_id):
     doc_id = decode_url(short_id)
     doc = get_object_or_404(Attachment, pk=doc_id)
 
-    document_url = '%s/%s' % (settings.VIEW_DOCUMENTS_BASE_URL, doc.attachment_file)
+    # temporary check for validation of alternative environment python3
+    date_start_fiadmin2 = datetime(2021, 7, 1, tzinfo=timezone.utc)
+    view_documents_url = settings.VIEW_DOCUMENTS_BASE_URL if doc.created_time > date_start_fiadmin2 else 'https://docs.bvsalud.org'
+
+    #document_url = '%s/%s' % (settings.VIEW_DOCUMENTS_BASE_URL, doc.attachment_file)
+    document_url = '%s/%s' % (view_documents_url, doc.attachment_file)
 
     return redirect(document_url)
