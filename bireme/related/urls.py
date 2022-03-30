34b70c7deaf9f129f6e62e4cf046ad4faf80acc1
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2022-03-30 14:20:55 -0300

    [biblioref] Refatoramento consulta relacionados (passivos)

--- bireme/related/urls.py
+++ bireme/related/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from related.views import *
 
 urlpatterns = [
-    re_path(r'^ajax/get_passive_relations/(?P<ctype_id>\d+)/(?P<obj_id>.+)/?$', get_passive_relations, name='get_passive_relations'),
+    re_path(r'^ajax/get_passive_relations/(?P<fiadmin_id>.+)/?$', get_passive_relations, name='get_passive_relations'),
 ]
