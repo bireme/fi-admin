d049cd3e223d82a9b2aea1cc9f48f5129234f3f2
wilsonmoura <wilsonsmoura@gmail.com>
2023-07-01 03:27:41 -0300

    #1395 -- Criar campo Modalidade do evento

--- bireme/events/search_indexes.py
+++ bireme/events/search_indexes.py
@@ -21,7 +21,7 @@ class EventIndex(indexes.SearchIndex, indexes.Indexable):
     thematic_area_display = indexes.MultiValueField()
     official_language = indexes.MultiValueField()
     official_language_display = indexes.MultiValueField()
-    event_modality = indexes.CharField()
+    event_modality = indexes.CharField(model_attr='event_modality', null=True)
     event_type = indexes.MultiValueField()
     descriptor = indexes.MultiValueField()
     keyword = indexes.MultiValueField()
