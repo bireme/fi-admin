f0297e1f60d42ee2185d4f043e4db0e427497af0
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2024-08-21 14:28:24 -0300

    [API] Adiciona campos pesquisa plugin title. #1459 #1461

--- bireme/title/search_indexes.py
+++ bireme/title/search_indexes.py
@@ -9,14 +9,22 @@ from django.contrib.contenttypes.models import ContentType
 class TitleIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
+    subtitle = indexes.CharField(model_attr='subtitle')
+    shortened_title = indexes.CharField(model_attr='shortened_title')
+    issn = indexes.CharField(model_attr='issn')
+    thematic_area = indexes.MultiValueField()
     status = indexes.CharField(model_attr='status')
     descriptor = indexes.MultiValueField()
     keyword = indexes.MultiValueField()
+    country = indexes.CharField()
     created_date = indexes.CharField()
     updated_date = indexes.CharField()
 
     def get_model(self):
         return Title
+
+    def get_updated_field(self):
+        return "updated_time"
 
     def prepare_status(self, obj):
         if obj.status == 'C':
@@ -30,6 +38,14 @@ class TitleIndex(indexes.SearchIndex, indexes.Indexable):
     def prepare_keyword(self, obj):
         return [keyword.text for keyword in Keyword.objects.filter(object_id=obj.id, content_type=ContentType.objects.get_for_model(obj), status=1)]
 
+    def prepare_country(self, obj):
+        if obj.country:
+            translations = obj.country.get_translations()
+            return "|".join(translations)
+
+    def prepare_thematic_area(self, obj):
+        return [line.strip() for line in obj.thematic_area.split('\n') if line.strip()]
+
     def prepare_created_date(self, obj):
         if obj.created_time:
             return obj.created_time.strftime('%Y%m%d')
@@ -42,5 +58,3 @@ class TitleIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.filter(created_time__lte=datetime.datetime.now())
 
-    def prepare_serial_type(self, obj):
-        return [ "|".join( event_type.get_translations() ) for event_type in EventType.objects.filter(event=obj.id) ]
